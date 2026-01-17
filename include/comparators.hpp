fa099b207dfa00b3c114c29e003008e5f5dd2e6b
kfadeev <kfadeev@scorewarrior.com>
2026-01-17 12:28:05 +0200

    Comparators added

--- include/comparators.hpp
+++ include/comparators.hpp
@@ -4,6 +4,16 @@
 
 namespace bookdb::comp {
 
-struct LessByAuthor {};
+struct LessByAuthor {
+    constexpr bool operator()(const Book &lhs, const Book &rhs) const { return lhs.author < rhs.author; }
+};
+
+struct LessByPopularity {
+    constexpr bool operator()(const Book &lhs, const Book &rhs) const { return lhs.read_count < rhs.read_count; }
+};
+
+struct LessByRating {
+    constexpr bool operator()(const Book &lhs, const Book &rhs) const { return lhs.rating < rhs.rating; }
+};
 
 }  // namespace bookdb::comp
