5713e4967a0ac6d967b026730282b6e42b48ec56
kfadeev <kfadeev@scorewarrior.com>
2026-01-17 12:39:36 +0200

    Concepts added

--- include/concepts.hpp
+++ include/concepts.hpp
@@ -2,24 +2,31 @@
 
 #include <concepts>
 #include <iterator>
+#include <ranges>
 
 #include "book.hpp"
 
 namespace bookdb {
 
 template <typename T>
-concept BookContainerLike = true;
+concept BookContainerLike = std::ranges::range<T> && std::same_as<std::ranges::range_value_t<T>, Book>;
 
 template <typename T>
-concept BookIterator = true;
+concept BookIterator = std::input_or_output_iterator<T> && requires(T it) {
+    { *it } -> std::convertible_to<const Book &>;
+};
 
 template <typename S, typename I>
-concept BookSentinel = true;
+concept BookSentinel = std::sentinel_for<S, I> && BookIterator<I>;
 
 template <typename P>
-concept BookPredicate = true;
+concept BookPredicate = requires(P pred, const Book &book) {
+    { pred(book) } -> std::convertible_to<bool>;
+};
 
 template <typename C>
-concept BookComparator = true;
+concept BookComparator = requires(C comp, const Book &a, const Book &b) {
+    { comp(a, b) } -> std::convertible_to<bool>;
+};
 
 }  // namespace bookdb
